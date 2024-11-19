221aabb280ddce30d1e742c0d30c1fb70a5f022d
JOÃO ARTHUR DOS SANTOS <joao_arthur-santos@estudante.sesisenai.org.br>
2024-11-18 21:26:01 -0300

    Realizado até a criação da classe Post

---
+++ src/com/redesocial/modelo/Post.java
@@ -0,0 +1,93 @@
+package com.redesocial.modelo;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Post {
+
+    private Integer id;
+    private List<Usuario> autor;
+    private String conteudo;
+
+    private LocalDateTime dataPublicacao;
+
+    private List<Usuario> curtidas;
+
+    private List<Comentario> comentarios;
+
+    public Post(Integer id, List<Usuario> autor, String conteudo, LocalDateTime dataPublicacao, List<Usuario> curtidas, List<Comentario> comentarios) {
+        this.id = id;
+        this.autor = autor;
+        this.conteudo = conteudo;
+        this.dataPublicacao = dataPublicacao;
+        this.curtidas = new ArrayList();
+        this.comentarios = new ArrayList();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<Usuario> getAutor() {
+        return autor;
+    }
+
+    public void setAutor(List<Usuario> autor) {
+        this.autor = autor;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public LocalDateTime getDataPublicacao() {
+        return dataPublicacao;
+    }
+
+    public void setDataPublicacao(LocalDateTime dataPublicacao) {
+        this.dataPublicacao = dataPublicacao;
+    }
+
+    public List<Usuario> getCurtidas() {
+        return curtidas;
+    }
+
+    public void setCurtidas(List<Usuario> curtidas) {
+        this.curtidas = curtidas;
+    }
+
+    public List<Comentario> getComentarios() {
+        return comentarios;
+    }
+
+    public void setComentarios(List<Comentario> comentarios) {
+        this.comentarios = comentarios;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Id: " + id +
+                "Autor: " + autor +
+                "Conteudo: " + conteudo + '\'' +
+                "DataPublicacao: " + dataPublicacao +
+                "Curtidas: " + curtidas +
+                "Comentarios: " + comentarios ;
+    }
+
+
+    public boolean adicionarCurtida(Usuario usuario) {
+        //Lista de Post
+
+    }
+
+}
---
+++ src/com/redesocial/modelo/Usuario.java
@@ -0,0 +1,151 @@
+package com.redesocial.modelo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Usuario {
+
+    private Integer id;
+    private String nome;
+    private String username;
+    private String email;
+    private String senha;
+    private LocalDateTime dataCadastro;
+    private List<Usuario> amigos;
+    private List<Post> posts;
+
+    public Usuario(String nome, String username, String email, String senha, LocalDateTime dataCadastro, List<Usuario> amigos, List<Post> posts) {
+        this.nome = nome;
+        this.username = username;
+        this.email = email;
+        this.senha = senha;
+        this.dataCadastro = dataCadastro;
+        this.amigos =  new ArrayList();
+        this.posts =  new ArrayList();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public List<Usuario> getAmigos() {
+        return amigos;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public void setAmigos(List<Usuario> amigos) {
+        this.amigos = amigos;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "Id: " + id +
+                "Nome: " + nome + '\'' +
+                "Username: " + username + '\'' +
+                "Email: " + email + '\'' +
+                "Senha: " + senha + '\'' +
+                "DataCadastro: " + dataCadastro +
+                "Amigos: " + amigos +
+                "Posts: " + posts ;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Usuario usuario)) return false;
+        return Objects.equals(getId(), usuario.getId()) && Objects.equals(getNome(), usuario.getNome()) && Objects.equals(getUsername(), usuario.getUsername()) && Objects.equals(getEmail(), usuario.getEmail()) && Objects.equals(getSenha(), usuario.getSenha()) && Objects.equals(getDataCadastro(), usuario.getDataCadastro()) && Objects.equals(getAmigos(), usuario.getAmigos()) && Objects.equals(getPosts(), usuario.getPosts());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getNome(), getUsername(), getEmail(), getSenha(), getDataCadastro(), getAmigos(), getPosts());
+    }
+
+    public boolean  adicionarAmigo  (Usuario amigo)  {
+        if(!amigos.contains(amigo)) {
+            amigos.add(amigo);
+            return true;
+        }
+        return false;
+    };
+
+    public boolean removerAmigo(Usuario amigo) {
+        if(amigos.contains(amigo)) {
+            amigos.remove(amigo);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean adicionarPost(Post post) {
+
+    if(post == null) {
+      return false;
+    }
+    return  posts.add(post);
+
+    }
+    
+    private boolean validarUsuario(Usuario usuario)  {
+
+        return usuario.getNome() != null && !usuario.getNome().isEmpty() &&
+                usuario.getUsername() != null && !usuario.getUsername().isEmpty() &&
+                usuario.getEmail() != null && !usuario.getEmail().isEmpty() &&
+                usuario.getSenha() != null && !usuario.getSenha().isEmpty();
+    }
+}
